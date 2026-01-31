6c4e5aead4b4b8684df6b9e0bb1a72c59380bf00
bryrybry <bryong0176@gmail.com>
2026-02-01 07:27:23 +0800

    removing unneeded files, so its ready for submission

--- .spyder-py3/history.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# *** Spyder Python Console History Log ***
-
-## ---(Tue Jan 27 13:39:09 2026)---
-%runfile C:/Users/eizin/.spyder-py3/temp.py --wdir
-%runfile C:/Users/eizin/.spyder-py3/untitled1.py --wdir
-%runfile C:/Users/eizin/.spyder-py3/untitled4.py --wdir
